9210a5001da18f4014d06614e533490bd20c15af
Oli <1024958@student.sae.edu.au>
2021-09-30 18:15:03 +1000

    added navmesh and comments
    Added the nav mesh components plugin, which we need for pathfinding
    
    also added comments to hub world AI script

--- Assets/HubWorldAI.cs
+++ Assets/HubWorldAI.cs
@@ -5,13 +5,12 @@ using UnityEngine.AI;
 
 public class HubWorldAI : MonoBehaviour
 {
+    //Call pathfinding agent
     public NavMeshAgent agent;
 
     public List<GameObject> checkpoints;
-    //public Transform checkpoint;
 
     public float speed = 1000f;
-
 
     public Vector3 walkPoint;
     public bool walkPointSet;
@@ -19,10 +18,10 @@ public class HubWorldAI : MonoBehaviour
 
     void Start()
     {
+        //fill list with all checkpoints in scene
         checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
     }
 
-    // Update is called once per frame
     void Update()
     {
         Patroling();
@@ -30,12 +29,14 @@ public class HubWorldAI : MonoBehaviour
 
     private void Patroling()
     {
+        // if no walkpoint, search for a new walkpoint
         if (!walkPointSet) SearchWalkPoint();
 
-
+        // if there is a walkpoint, go there
         if (walkPointSet)
             agent.SetDestination(walkPoint);
 
+        // Speed controls - maybe where to look to change handling?
         Vector3 distanceToWalkPoint = (transform.position - walkPoint) * speed * Time.deltaTime;
 
         //walk point reached
@@ -44,15 +45,14 @@ public class HubWorldAI : MonoBehaviour
     }
     private void SearchWalkPoint()
     {
-        // calculate random point in range
+        // choose random checkpoint
         float newCheckpoint = Random.Range(0, GameObject.FindGameObjectsWithTag("Checkpoint").Length + 1);
 
+        //assign walkpoint to chosen checkpoint
         for (int i = 0; i < newCheckpoint; i++)
         {
             walkPoint = checkpoints[i].transform.position;
         }
-
-        
 
         walkPointSet = true;
     }
