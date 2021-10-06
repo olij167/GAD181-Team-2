939711421cad2aa400e0a454e608c9ea74a2ea81
Oli <1024958@student.sae.edu.au>
2021-10-06 22:06:49 +1100

    Added Races and Race AI

---
+++ Assets/Script/CheckpointTrigger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointTrigger : MonoBehaviour
+{
+    // race set-up variables
+    public string raceName;
+
+    public int raceCost, racerNum, numOfLaps;
+
+    public List<GameObject> raceCheckpoints;
+    
+}
