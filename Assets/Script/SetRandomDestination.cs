dc4642258cde55f9d0286025285d625922c51bad
Oli <1024958@student.sae.edu.au>
2021-11-01 23:39:36 +1100

    integrated pizza launcher to deliveries

--- Assets/Script/SetRandomDestination.cs
+++ Assets/Script/SetRandomDestination.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SetRandomDestination : MonoBehaviour
 {
@@ -8,12 +9,23 @@ public class SetRandomDestination : MonoBehaviour
     public GameObject destination; // selected destination
 
     public float deliveryRange; // look radius
-    public bool destinationSet, currentDeliveryComplete, destinationInRange;
-    public LayerMask destinationLayer;
+    public bool destinationSet, destinationInRange; //check if destination is set, check if destination is in range
+    public LayerMask destinationLayer; 
 
     public int numOfDeliveries, deliveryCounter;
 
+    // projectile variables
+    public Transform shootPos;
+    public GameObject pizza;
+    public List<GameObject> pizzaList; // control pizzas
+    public float shotPower, stopDistance; // pizza speed, pizza delivered proximity
+
     int destinationNum;
+
+    // delivery complete placeholder UI
+    public TextMeshProUGUI deliveriesCompleteUI;
+
+
     void Start()
     {
         destinationNum = GameObject.FindGameObjectsWithTag("Building").Length;
@@ -33,7 +45,10 @@ public class SetRandomDestination : MonoBehaviour
 
         destinationInRange = Physics.CheckSphere(transform.position, deliveryRange, destinationLayer); // check if destination is within delivery range
 
-        if (destinationInRange) DeliveryComplete(); //EngagePizzaLauncher();
+        //if (destinationInRange) 
+            EngagePizzaLauncher(); // press space to shoot pizza
+
+        deliveriesCompleteUI.text = deliveryCounter.ToString();
 
 
     }
@@ -51,14 +66,49 @@ public class SetRandomDestination : MonoBehaviour
 
     void EngagePizzaLauncher()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(pizza, shootPos.position, shootPos.rotation);
 
+            for (int i = 0; i < GameObject.FindGameObjectsWithTag("Pizza").Length; i++)
+            {
+                GameObject newPizza = GameObject.FindGameObjectsWithTag("Pizza")[i];
+
+                if (!pizzaList.Contains(newPizza))
+                {
+                    pizzaList.Add(newPizza); // add new pizza to list
+                }
+            }
+        }
+
+        foreach (GameObject pizza in pizzaList)
+        {
+            pizza.transform.position = Vector3.MoveTowards(pizza.transform.position, destination.transform.position, shotPower * Time.deltaTime); // send pizza to the destination
+
+            Vector3 distanceToWalkPoint = pizza.transform.position - destination.transform.position;
+
+            if (distanceToWalkPoint.magnitude < stopDistance) // check if pizza is at destination
+            {
+                DeliveryComplete();
+            }
+        }
     }
 
     void DeliveryComplete()
     {
-
         deliveryCounter++;
+
+        foreach (GameObject pizza in pizzaList)
+            Destroy(pizza);
+
+        pizzaList.Clear();
+
         SetDestination();
+
+        if (deliveryCounter >= numOfDeliveries)
+        {
+            //game over scene transition
+        }
     }
 
     void OnDrawGizmosSelected()
