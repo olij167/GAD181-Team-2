d9601a69e001389cf2bf114ac4525e4c38bc5006
Jericho <Jerichomccredie@outlook.com>
2021-11-16 19:53:55 +1100

    Game Over Added

--- Assets/Script/Condition.cs
+++ Assets/Script/Condition.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Condition : MonoBehaviour
 {
@@ -14,18 +15,31 @@ public class Condition : MonoBehaviour
     }
 private void OnCollisionEnter(Collision collision)
     {
+        //Cars driving around
         if (collision.gameObject.CompareTag("HubWorldCar"))
         {
             maxCondition = maxCondition - 5;
         }
+        //little guys walking around
         else if (collision.gameObject.CompareTag("Pedestrian"))
         {
             maxCondition = maxCondition - 1;
         }
+        //Buildins what can be delivered to
         else if (collision.gameObject.CompareTag("Building"))
         {
             maxCondition = maxCondition - 5;
         }
+        //randoms buildings/environment around town
+        else if (collision.gameObject.CompareTag("Decoration"))
+        {
+            maxCondition = maxCondition - 5;
+        }
         mytext.text = maxCondition.ToString();
+        //below is the code to load the game over screen
+        if (maxCondition < 0)
+        {
+            SceneManager.LoadScene("LoseScreen");
+        }
     }
 }
