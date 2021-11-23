9277167c5f036d797123b0d6d53bf83593c02db4
John <johnbaraceros13@gmail.com>
2021-11-24 01:26:59 +1100

    Stages - extra song

--- Assets/Audio Assets/AudioPlayer.cs
+++ Assets/Audio Assets/AudioPlayer.cs
@@ -8,14 +8,18 @@ public class AudioPlayer : MonoBehaviour
     public Mis1eader.MediaPlayer.AudioSourcePlayer mediaPlayer = null;
     //public Mis1eader.MediaPlayer.VideoClipPlayer mediaPlayer = null;
     //public Mis1eader.MediaPlayer.VideoSourcePlayer mediaPlayer = null;
-    public UnityEngine.KeyCode backward = UnityEngine.KeyCode.E;
-    public UnityEngine.KeyCode forward = UnityEngine.KeyCode.Q;
+    public UnityEngine.KeyCode backward = UnityEngine.KeyCode.Q;
+    public UnityEngine.KeyCode forward = UnityEngine.KeyCode.R;
+    public UnityEngine.KeyCode pause = UnityEngine.KeyCode.W;
+    public UnityEngine.KeyCode play = UnityEngine.KeyCode.E;
     private void Update()
     {
         if (mediaPlayer)
         {
             if (UnityEngine.Input.GetKeyDown(backward)) mediaPlayer.SkipBackward();
             else if (UnityEngine.Input.GetKeyDown(forward)) mediaPlayer.SkipForward();
+            else if (UnityEngine.Input.GetKeyDown(pause)) mediaPlayer.Pause();
+            else if (UnityEngine.Input.GetKeyDown(play)) mediaPlayer.Play();
         }
     }
 }
