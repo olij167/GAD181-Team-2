9543673fa9499f3c6c8948e52b3d87acdbedfe04
Oli <1024958@student.sae.edu.au>
2021-11-24 11:30:28 +1100

    pedestrian collision feedback

---
+++ Assets/Script/PedestrianFeedback.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianFeedback : MonoBehaviour
+{
+    public Material badFeedback;
+    Material originalHeadMaterial;
+    Color originalColour;
+    GameObject child;
+    public bool playerFeedback;
+    public float feedbackTimer, feedbackTimerReset;
+
+    void Start()
+    {
+        child = gameObject.transform.GetChild(0).gameObject;
+        originalHeadMaterial = child.GetComponent<MeshRenderer>().material;
+        originalColour = gameObject.GetComponent<MeshRenderer>().material.color;
+    }
+
+    void Update()
+    {
+        if (playerFeedback)
+        {
+            child.GetComponent<MeshRenderer>().material = badFeedback;
+            gameObject.GetComponent<MeshRenderer>().material.color = badFeedback.color;
+
+            feedbackTimer -= Time.deltaTime;
+
+            if (feedbackTimer <= 0)
+            {
+                child.GetComponent<MeshRenderer>().material = originalHeadMaterial;
+                gameObject.GetComponent<MeshRenderer>().material.color = originalColour;
+                feedbackTimer = feedbackTimerReset;
+                playerFeedback = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            playerFeedback = true;
+        }
+
+        if (collision.gameObject.tag.Equals("Pizza"))
+        {
+            playerFeedback = true;
+            Destroy(collision.gameObject);
+        }
+    }
+}
