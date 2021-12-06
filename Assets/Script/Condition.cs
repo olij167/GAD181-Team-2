4af154d7964a87c31de4b5f084097659a6673d9b
Oli <1024958@student.sae.edu.au>
2021-12-06 12:53:07 +1100

    fix merge issues

--- Assets/Script/Condition.cs
+++ Assets/Script/Condition.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-//using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class Condition : MonoBehaviour
 {
